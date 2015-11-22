ee9f30c78fa9ca8b868a024a93bc6a24aa8bc0dd
Rosslaew <rosslaew@gmail.com>
2015-11-22 20:13:59 +0200

    Modifications of classes Gear and Gearset.

--- gear/__init__.py
+++ gear/__init__.py
@@ -1,5 +1,96 @@
+from collections import UserList
+import power as p
+
+"""Global definition of attributes and gear slots."""
+__slots__ = (
+    "weapon",
+    "head",
+    "body",
+    "belt",
+    "legs",
+    "feet",
+    "shield",
+    "neck",
+    "ears",
+    "arm",
+    "ring1",
+    "ring2")
+
+__attributes__ = (
+    "weapon_damage",
+    "auto_attack",
+    "delay",
+    "block_strength",
+    "block_rate",
+    "physical_defense",
+    "magical_defense",
+    "dexterity",
+    "strength",
+    "mind",
+    "intelligence",
+    "vitality",
+    "critical",
+    "determination",
+    "skill_speed",
+    "spell_speed",
+    "accuracy",
+    "parry",
+    "piety"
+        )
+
+"""Class representing a simple gear element.
+"""
 class Gear(object):
-    def __init__(self, slot, **attributes):
-        self.slot = slot
-        for k,v in attributes.items():
-            self.__setattr__(k,v)
+    def __init__(self, slot, item_id = None, **attributes):
+        if item_id is None :
+            self.slot = slot
+            for att in __attributes__:
+                self.__setattr__(att,attributes[att])
+        else:
+            pass #TODO add fetching gear from xivdb
+
+"""Class representing a complete gear set.
+    Can be called by specifying the Lodestone ID for
+    the character or by specifying the gear
+    for each gear slot.
+
+    Available slots :
+    weapon
+    head
+    body
+    belt
+    legs
+    feet
+    shield
+    neck
+    ears
+    arm
+    ring1
+    ring2
+"""
+class GearSet(Gear):
+    def __init__(self, 
+            character_id=None,
+            **gears
+            ):
+        self.gears = {}
+        if character_id is None:
+            for s in __slots__:
+                self.gear[s] = gears[s]
+                self.gear[s].slot = s
+        else:
+            pass #TODO add fetching gearset from Lodestone
+
+        # A GearSet is treated as a Gear, so we update the attributes
+        attributes = {} # TODO find an elegant way to do this
+        for g in gears:
+            pass
+
+
+
+class GearSetList(UserList):
+    def __init__(self, data=[]):
+        super().__init__(data)
+
+    def maxPower(self,job, constraintList=None):
+        pass
---
+++ gear/power.py
@@ -0,0 +1,17 @@
+"""Table of attribute weights
+"""
+__weights__ = {
+
+}
+
+"""Function to get the weight of the attributes
+of a certain job.
+"""
+def weights(job):
+    return __weights__[job]
+
+"""Function to calculate the power of a gear for job.
+"""
+def power(gear, job):
+    pass
+
